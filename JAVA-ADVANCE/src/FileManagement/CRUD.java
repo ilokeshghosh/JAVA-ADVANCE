87c5428a363fe4c39e89e5b37a9cb5122642b3cd
Lokesh Ghosh <lghosh857@gmail.com>
2023-01-22 20:14:58 +0530

    updated CRUD.java

--- JAVA-ADVANCE/src/FileManagement/CRUD.java
+++ JAVA-ADVANCE/src/FileManagement/CRUD.java
@@ -37,7 +37,14 @@ public class CRUD {
             System.out.println("2.Display");
             System.out.println("3.Search");
             System.out.println("4.Delete");
-            System.out.println("0. Exit");
+            System.out.println("5.Update");
+            System.out.println("6.Sort By EmpNo - On Screen");
+            System.out.println("7.Sort By EmpNo - In File");
+            System.out.println("8.Sort By EmpName - On Screen");
+            System.out.println("9.Sort By EmpName - In File");
+            System.out.println("10.Sort By Salary - Desc - On Screen");
+            System.out.println("11.Sort By Salary - Asc - In File");
+            System.out.println("0.Exit");
             System.out.println("Enter you choice");
             choice= sc.nextInt();
             switch (choice){
@@ -108,9 +115,249 @@ public class CRUD {
                     else {
                         System.out.println("File Not Exist......!!");
                     }
+                    break;
 
+                case 4:
+                    if(file.isFile()){
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al=(ArrayList<Employee>)ois.readObject();
+                        ois.close();
+                        boolean found=false;
+                        System.out.println("Enter Employee Number to Delete");
+                        empNo = sc.nextInt();
+                        System.out.println("----------------------------------------");
+                        System.out.println("Emp-id Emp-Name Emp-salary");
+                        li= al.listIterator();
+                        while (li.hasNext()){
+
+                            Employee e= (Employee) li.next();
+                            if(e.empNo == empNo){
+                                li.remove();
+                                found = true;
+                            }
+
+
+                        }
+                        if(found){
+                            oos=new ObjectOutputStream(new FileOutputStream(file));
+                            oos.writeObject(al);
+                            oos.close();
+                            System.out.println("Record Deleted Successfully......!");
+                        }
+                        else{
+                            System.out.println("Record Not Found......!");
+                        }
+                        System.out.println("----------------------------------------");
+                    }
+                    else {
+                        System.out.println("File Not Exist......!!");
+                    }
+                    break;
+
+                case 5:
+                    if(file.isFile()){
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al=(ArrayList<Employee>)ois.readObject();
+                        ois.close();
+                        boolean found=false;
+                        System.out.println("Enter Employee Number to Update");
+                        empNo = sc.nextInt();
+                        System.out.println("----------------------------------------");
+                        System.out.println("Emp-id Emp-Name Emp-salary");
+                        li= al.listIterator();
+                        while (li.hasNext()){
+
+                            Employee e= (Employee) li.next();
+                            if(e.empNo == empNo){
+                                System.out.print("Enter New EmpName : ");
+                                String empName= sc1.nextLine();
+
+                                System.out.print("Enter new Salary : ");
+                                int salary= sc.nextInt();
+
+                                li.set(new Employee(empNo, empName, salary));
+                                found = true;
+                            }
+
+
+                        }
+                        if(found){
+                            oos=new ObjectOutputStream(new FileOutputStream(file));
+                            oos.writeObject(al);
+                            oos.close();
+                            System.out.println("Record Updated Successfully......!");
+                        }
+                        else{
+                            System.out.println("Record Not Found......!");
+                        }
+                        System.out.println("----------------------------------------");
+                    }
+                    else {
+                        System.out.println("File Not Exist......!!");
+                    }
+                    break;
+
+                case 6:
+                    if(file.isFile()){
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al=(ArrayList<Employee>)ois.readObject();
+                        ois.close();
+
+                        Collections.sort(al, new Comparator<Employee>() {
+                            @Override
+                            public int compare(Employee o1, Employee o2) {
+                                return o1.empNo - o2.empNo;
+                            }
+                        });
+                        System.out.println("----------------------------------------");
+                        System.out.println("Emp-id Emp-Name Emp-salary");
+                        li= al.listIterator();
+                        while (li.hasNext()){
+                            System.out.println(li.next());
+                        }
+                        System.out.println("----------------------------------------");
+                    }else {
+                        System.out.println("File Not Exist......!!");
+                    }
 
                     break;
+                case 7:
+                    if(file.isFile()){
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al=(ArrayList<Employee>)ois.readObject();
+                        ois.close();
+
+                        Collections.sort(al, new Comparator<Employee>() {
+                            @Override
+                            public int compare(Employee o1, Employee o2) {
+                                return o1.empNo - o2.empNo;
+                            }
+                        });
+
+                        oos = new ObjectOutputStream(new FileOutputStream(file));
+                        oos.writeObject(al);
+                        oos.close();
+                        System.out.println("----------------------------------------");
+                        System.out.println("Emp-id Emp-Name Emp-salary");
+                        li= al.listIterator();
+                        while (li.hasNext()){
+                            System.out.println(li.next());
+                        }
+                        System.out.println("----------------------------------------");
+                    }else {
+                        System.out.println("File Not Exist......!!");
+                    }
+
+                    break;
+                case 8:
+                    if(file.isFile()){
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al=(ArrayList<Employee>)ois.readObject();
+                        ois.close();
+
+                        Collections.sort(al, new Comparator<Employee>() {
+                            @Override
+                            public int compare(Employee o1, Employee o2) {
+                                return o1.empName.compareTo(o2.empName);
+                            }
+                        });
+                        System.out.println("----------------------------------------");
+                        System.out.println("Emp-id Emp-Name Emp-salary");
+                        li= al.listIterator();
+                        while (li.hasNext()){
+                            System.out.println(li.next());
+                        }
+                        System.out.println("----------------------------------------");
+                    }else {
+                        System.out.println("File Not Exist......!!");
+                    }
+
+                    break;
+                case 9:
+                    if(file.isFile()){
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al=(ArrayList<Employee>)ois.readObject();
+                        ois.close();
+
+                        Collections.sort(al, new Comparator<Employee>() {
+                            @Override
+                            public int compare(Employee o1, Employee o2) {
+                                return o1.empName.compareTo(o2.empName);
+                            }
+                        });
+
+                        oos = new ObjectOutputStream(new FileOutputStream(file));
+                        oos.writeObject(al);
+                        oos.close();
+                        System.out.println("----------------------------------------");
+                        System.out.println("Emp-id Emp-Name Emp-salary");
+                        li= al.listIterator();
+                        while (li.hasNext()){
+                            System.out.println(li.next());
+                        }
+                        System.out.println("----------------------------------------");
+                    }else {
+                        System.out.println("File Not Exist......!!");
+                    }
+
+                    break;
+
+                case 10:
+                    if(file.isFile()){
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al=(ArrayList<Employee>)ois.readObject();
+                        ois.close();
+
+                        Collections.sort(al, new Comparator<Employee>() {
+                            @Override
+                            public int compare(Employee o1, Employee o2) {
+                                return o2.salary-o1.salary;
+                            }
+                        });
+                        System.out.println("----------------------------------------");
+                        System.out.println("Emp-id Emp-Name Emp-salary");
+                        li= al.listIterator();
+                        while (li.hasNext()){
+                            System.out.println(li.next());
+                        }
+                        System.out.println("----------------------------------------");
+                    }else {
+                        System.out.println("File Not Exist......!!");
+                    }
+
+                    break;
+                case 11:
+                    if(file.isFile()){
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al=(ArrayList<Employee>)ois.readObject();
+                        ois.close();
+
+                        Collections.sort(al, new Comparator<Employee>() {
+                            @Override
+                            public int compare(Employee o1, Employee o2) {
+                                return o1.salary-o2.salary;
+                            }
+                        });
+
+                        oos = new ObjectOutputStream(new FileOutputStream(file));
+                        oos.writeObject(al);
+                        oos.close();
+                        System.out.println("----------------------------------------");
+                        System.out.println("Emp-id Emp-Name Emp-salary");
+                        li= al.listIterator();
+                        while (li.hasNext()){
+                            System.out.println(li.next());
+                        }
+                        System.out.println("----------------------------------------");
+                    }else {
+                        System.out.println("File Not Exist......!!");
+                    }
+
+                    break;
+
+
+
+
             }
         }while (choice !=0);
     }
